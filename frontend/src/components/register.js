c2ec2d24b7634c32607f507a0a028ec1c11535ee
Tushar-Biswas <tusharbiswas1014gmail.com>
2023-09-03 22:19:39 +0600

    ajax email check added

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -3,15 +3,15 @@ import {useHistory} from 'react-router-dom'
 function Register() {
 
   
-
-  const [firstName,setFirstName]=useState("")
-  const [lastName,setLastName]=useState("")
-  const [address,setAddress]=useState("")
-  const [phone,setPhone]=useState("")
-  const [email,setEmail]=useState("")
-  const [birthday,setBirthday]=useState("")
-  const [password,setPassword]=useState("")
-  const history=useHistory();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [address, setAddress] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailAvailable, setEmailAvailable] = useState(true); // State to track email availability
+  const history = useHistory();
 
   useEffect(() => {
     if (localStorage.getItem('user-info')) {
@@ -19,27 +19,49 @@ function Register() {
     }
   }, [history]);
 
-  async function signUp(){
-    let item={firstName, lastName,address, phone, email, birthday, password}
-    //console.warn(item)
-    let result = await fetch("http://localhost:8000/api/register",{
-      method:"POST",
-      body:JSON.stringify(item),
-      headers:{
-        "Content-Type":'application/json',
-        "Accept":'application/json'
-      }
-    })
+  // Function to check email availability
+  async function checkEmailAvailability(email) {
+    try {
+      const response = await fetch(`http://localhost:8000/api/check-email?email=${email}`);
+      const data = await response.json();
+      return data.available;
+    } catch (error) {
+      console.error('Error checking email availability:', error);
+      return false;
+    }
+  }
 
-    result = await result.json()
-    // localStorage.setItem("user-info", JSON.stringify(result))
-    // history.push("/profile")
-    if (email === "admin@localhost.local" && password === "admin") {
-      localStorage.setItem("user-info", JSON.stringify(result));
-      history.push("/admin"); // Redirect to admin page
-    } else {
-      localStorage.setItem("user-info", JSON.stringify(result));
-      history.push("/profile"); // Redirect to profile page
+  // Function to handle registration
+  async function signUp() {
+    const isEmailAvailable = await checkEmailAvailability(email);
+
+    if (!isEmailAvailable) {
+      alert('Email is already registered. Please use a different email.');
+      return;
+    }
+
+    const item = { firstName, lastName, address, phone, email, birthday, password };
+
+    try {
+      let result = await fetch('http://localhost:8000/api/register', {
+        method: 'POST',
+        body: JSON.stringify(item),
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+
+      result = await result.json();
+      localStorage.setItem('user-info', JSON.stringify(result));
+
+      if (email === 'admin@localhost.local' && password === 'admin') {
+        history.push('/admin');
+      } else {
+        history.push('/profile');
+      }
+    } catch (error) {
+      console.error('Error registering user:', error);
     }
   }
 
@@ -54,8 +76,21 @@ function Register() {
         <br/>
         <input type='text' value={phone} onChange={(e)=>setPhone(e.target.value)} className='form-control' placeholder='Phone'/>
         <br/>
-        <input type='text' value={email} onChange={(e)=>setEmail(e.target.value)} className='form-control' placeholder='Email'/>
-        <br/>
+        <input
+        type="text"
+        value={email}
+        onChange={async (e) => {
+          setEmail(e.target.value);
+          const isAvailable = await checkEmailAvailability(e.target.value);
+          setEmailAvailable(isAvailable);
+        }}
+        className={`form-control ${!emailAvailable ? 'is-invalid' : ''}`}
+        placeholder="Email"
+      />
+      {!emailAvailable && (
+        <div className="invalid-feedback">Email is already registered. Please use a different email.</div>
+      )}
+      <br/>
         <input type='text' value={birthday} onChange={(e)=>setBirthday(e.target.value)} className='form-control' placeholder='Birthday'/>
         <br/>
         <input type='text' value={password} onChange={(e)=>setPassword(e.target.value)} className='form-control' placeholder='Password'/>

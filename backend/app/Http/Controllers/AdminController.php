5a01e106bef441c14cb763ad505d1940fcdfd6a3
Tushar-Biswas <tusharbiswas1014gmail.com>
2023-09-03 20:00:38 +0600

    admin page frontend and backend added

---
+++ backend/app/Http/Controllers/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Admin;
+class AdminController extends Controller
+{
+    // function list(){
+    //     return Admin::all();
+    // }
+ }
